d458a8ace594387514e24e43dad63d571a9b9cae
MA__AM <qyh.1515212247@outlook.com>
2023-12-14 15:09:38 +0800

    网站名更改测试

---
+++ scripts/main01.js
@@ -0,0 +1,10 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let myVariable = "李雷";
+myVariable = "韩梅梅";
+
+//()=>等同于function() 匿名函数
+document.querySelector("html").addEventListener("click",function(){
+    alert("别戳我，我怕疼。");
+});
